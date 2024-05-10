c62b344cce61eec4392e3c68042ff39867c6591b
artem nehsko <asneshko@mts.ru>
2024-05-10 19:11:06 +0400

    создан проект, добавлен гит игнор, поды

---
+++ Podfile
@@ -0,0 +1,9 @@
+use_frameworks!
+platform :ios, '15'
+
+
+target 'Poliv-ios' do
+    pod 'SnapKit'
+    pod 'SQLite.swift'
+    pod 'Alamofire'
+end
---
+++ Poliv-ios/Poliv_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poliv_iosApp.swift
+//  Poliv-ios
+//
+//  Created by artem on 10.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Poliv_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
